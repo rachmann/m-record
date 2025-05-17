4ce9a1c412d22f2a7cb2fc45acce6acf7b6d5188
Robert A <achmann@outlook.com>
2025-05-17 19:58:47 -0300

    screen capture, but need to make key log always write to file

---
+++ m-record/EnumExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace m_record
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescription(this Enum value)
+        {
+            if (value == null)
+                return string.Empty;
+
+            FieldInfo? fi = value.GetType().GetField(value.ToString());
+            if (fi != null)
+            {
+                var attr = fi.GetCustomAttribute<DescriptionAttribute>();
+                if (attr != null)
+                    return attr.Description;
+            }
+            return value.ToString();
+        }
+
+        public static TEnum FromDescription<TEnum>(string description) where TEnum : struct, Enum
+        {
+            foreach (var field in typeof(TEnum).GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+                {
+                    if (attr.Description == description)
+                        return (TEnum)field.GetValue(null)!;
+                }
+                else
+                {
+                    if (field.Name == description)
+                        return (TEnum)field.GetValue(null)!;
+                }
+            }
+            // Return default (0) if no match found
+            return default;
+        }
+    }
+}
